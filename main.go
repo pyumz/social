5ebee814ab5c3163231ad17890c67b5d5101d83c
Peter Yum <pyum1013@gmail.com>
2022-05-16 17:00:18 -0400

    Add endpoint method

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -14,9 +15,32 @@ type errorBody struct {
 	Error string `json:"error"`
 }
 
-func main() {
+type apiConfig struct {
+	dbClient database.Client
+}
 
+func main() {
 	m := http.NewServeMux()
+
+	dbClient := database.NewClient("db.json")
+	err := dbClient.EnsureDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	apiCfg := apiConfig{
+		dbClient: dbClient,
+	}
+
+	m.HandleFunc("/users", apiCfg.endpointUsersHandler)
+	m.HandleFunc("/users/", apiCfg.endpointUsersHandler)
+
+	// fmt.Println("HandleFunc method called ")
+	// http.HandleFunc("/", testHandler)
+
+	// fmt.Println("HandleFunc Error method called")
+	// http.HandleFunc("/err", testErrHandler)
+
 	const addr = "localhost:8081"
 
 	srv := http.Server{
@@ -26,26 +50,20 @@ func main() {
 		ReadTimeout:  30 * time.Second,
 	}
 
-	fmt.Println("HandleFunc method called ")
-	http.HandleFunc("/", testHandler)
-
-	fmt.Println("HandleFunc Error method called")
-	http.HandleFunc("/err", testErrHandler)
-
-	fmt.Println("listening...")
+	fmt.Println("listening on", srv.Addr)
 	http.ListenAndServe(srv.Addr, nil)
 	fmt.Println("Error is ", http.ListenAndServe(srv.Addr, nil))
 }
 
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, 200, database.User{
-		Email: "test@browser.com",
-	})
-}
+// func testHandler(w http.ResponseWriter, r *http.Request) {
+// 	respondWithJSON(w, 200, database.User{
+// 		Email: "test@browser.com",
+// 	})
+// }
 
-func testErrHandler(w http.ResponseWriter, r *http.Request) {
-	respondWithError(w, 500, errors.New("Server error"))
-}
+// func testErrHandler(w http.ResponseWriter, r *http.Request) {
+// 	respondWithError(w, 500, errors.New("Server error"))
+// }
 
 func respondWithError(w http.ResponseWriter, code int, err error) {
 	respondWithJSON(w, code, errorBody{
@@ -68,4 +86,16 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 
 	w.WriteHeader(code)
 	w.Write(response)
+}
+
+func (apiCfg apiConfig) endpointUsersHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+	case http.MethodPost:
+		apiCfg.handlerCreateUser(w, r)
+	case http.MethodPut:
+	case http.MethodDelete:
+	default:
+		respondWithError(w, 404, errors.New("Unsupported API method call"))
+	}
 }
